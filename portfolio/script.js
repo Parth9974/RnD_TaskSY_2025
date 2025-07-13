bd76c72564c653d8e66bc3f824c1fd3773bbb501
Parth Zutshi <parthzutshi6@gmail.com>
2025-07-13 21:15:10 +0530

    Added final portfolio with images and assets

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+  window.onload = () => {
+    window.scrollTo(0, 0); // Force scroll to top on reload
+  };    
+     const typed = new Typed(".element", {
+            strings: ["a Web Developer", "a Programmer", "exploring Problem Solving", "a Tech Enthusiast"],
+            typeSpeed: 50,
+            backSpeed: 40,
+            loop: true
+        });
